d70898d7475e3b960726fc8326715f0f015a0166
semal259 <semalpatel2596@gmail.com>
2019-02-23 13:48:22 -0800

    add initial command line argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+extern crate clap;
+
+use clap::{Arg, App};
+
 fn main() {
-    println!("Hello, world!");
+        let matches = App::new("bull")
+                    .version("0.1.0")
+                    .author("Semal Patel <semalpatel2596@gmail.com>")
+                    .about("stocks from the cli")
+                    .arg(Arg::with_name("stock name")
+                                .required(true)
+                                .takes_value(true)
+                                .index(1)
+                                .help("get price by name"))
+                    .get_matches();
+        let stock_name = matches.value_of("stock name").unwrap();
+        println!("{}", stock_name);
 }
